46d554dd465db6fef74ec6955ff5b63257933d2b
NafiAtero <magfarnafi@gmail.com>
2023-12-28 10:45:59 +0700

    fixed Order

--- src/BE/Order.java
+++ src/BE/Order.java
@@ -2,17 +2,60 @@ package BE;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Timestamp;
 
 
 
 public class Order {
     private int orderId, outletId;
     private String tableName;
-    // datetime
+    private Timestamp orderTime;
     private final List<OrderProduct> orderProducts = new ArrayList<>();
-    //todo constructor
+    public Order(int orderId, int outletId, String tableName, Timestamp orderTime) {
+        this.orderId = orderId;
+        this.outletId = outletId;
+        this.tableName = tableName;
+        this.orderTime = orderTime;
+    }
 
 //region Setter Getter
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getOutletId() {
+        return outletId;
+    }
+
+    public void setOutletId(int outletId) {
+        this.outletId = outletId;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public Timestamp getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Timestamp orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public List<OrderProduct> getOrderProducts() {
+        return orderProducts;
+    }
+
 //endregion
 
     public void addItem(int productId, int companyId, String name, int price, int orderProductId, int outletProductId, int quantity) {
